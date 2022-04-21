c99574aabdf6f4dd6b3db5c0235f205fa7afb1af
Tomáš Konvička <tomasin.konvicka@email.cz>
2022-04-21 15:22:12 +0200

    Projekt

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include "stm8s.h"
 #include "milis.h"
 #include "spse_stm8.h"
-#include "stm8_hd44780.h"
+//#include "stm8_hd44780.h"
 
 #include "delay.h"
 #include "stdio.h"
 //#include "../lib/uart.c"*/
 #include  "assert.h"
+
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
@@ -21,24 +22,21 @@
 #define BTN_PIN  GPIO_PIN_4
 #define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET)
 
-#define START_PORT GPIOB
-#define START_PIN GPIO_PIN_1
-#define KONEC_PORT GPIOB
-#define KONEC_PIN GPIO_PIN_2
+#define BRANA_PORT GPIOB
+#define BRANA_PIN GPIO_PIN_1
+#define BRANA GPIO_ReadInputPin(BRANA_PORT, BRANA_PIN)
 
-#define BRANA_START GPIO_ReadInputPin(START_PORT, START_PIN)
-#define BRANA_KONEC GPIO_ReadInputPin(KONEC_PORT, KONEC_PIN)
+#define BZUCAK_PORT GPIOB
+#define BZUCAK_PIN GPIO_PIN_3
+#define bzuci GPIO_WriteHigh(BZUCAK_PORT, BZUCAK_PIN)
+#define nebzuci GPIO_WriteLow(BZUCAK_PORT ,BZUCAK_PIN)
 
 uint32_t time1 = 0;
 uint32_t time2 = 0;
-uint32_t time3 = 0;
-uint32_t time4 = 0;
-uint32_t time5 = 0;
-uint32_t cas = 0;
-uint32_t milisekundy = 0;
-uint32_t sekundy = 0;
-_Bool casovac = 0;
-char text[32];
+_Bool aktualni = 0;
+_Bool minuly = 0;
+_Bool zmena = 0;
+uint8_t pocet = 0;
 
 
 void setup(void)
@@ -46,54 +44,45 @@ void setup(void)
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-    GPIO_Init(START_PORT, START_PIN, GPIO_MODE_IN_FL_NO_IT);
-    GPIO_Init(KONEC_PORT, KONEC_PIN, GPIO_MODE_IN_FL_NO_IT);
-    lcd_init();
+    GPIO_Init(BRANA_PORT, BRANA_PIN, GPIO_MODE_IN_FL_NO_IT);
+    GPIO_Init(BZUCAK_PORT, BZUCAK_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     init_milis();
     
-    //init_uart1();
-}
-uint32_t stopky(void){
-    cas=milis();
-    if (cas - milis() >= 1000){
-        sekundy++;
-    }
-    if (cas - milis() >= 1){
-        milisekundy++;
-        if (milisekundy == 1000){
-            milisekundy=0;
-        }
-    }
-    lcd_gotoxy(0,0);
-    sprintf(text,"Čas: %2u",sekundy);
-    lcd_puts(text);
-    lcd_gotoxy(0,6);
-    sprintf(text,":%3u",milisekundy);
-
+    //init_uart();
 }
 
-void main(void)
+int main(void)
 {
   
     setup();
 
     while (1) {
-
-        if (BRANA_START && milis()-time1 > 200){
-           casovac=1;  
+        if (milis() - time1 > 10){
+            time1=milis();
+            if(BRANA){
+                aktualni=1;
+            }else{
+                aktualni=0;
+            }
+        if (minuly==1 && aktualni==0){
+            minuly=0;
+            pocet++;
+            zmena=1;
         }
-        if (BRANA_KONEC && milis()-time2 > 5){
-            casovac=0;
+        minuly = aktualni;
         }
-        if (casovac==1 && milis()-time3 > 5){
-            stopky();
+        if (zmena==1 && milis()-time2 > 150){
+            time2 = milis();
+            //prinf("Počet: %ld \n \r",pocet);
+            bzuci;
+            delay_ms(200);
+            nebzuci;
+            zmena=0;
         }
-        if (BTN_PUSH && milis()-time4 > 200 ){
-            sekundy = 0;
-            milisekundy = 0; 
+        if (BTN_PUSH){
+            pocet=0;
+            zmena=1;
         }
-        if (milis()-time5 > 500)
-            lcd_clear();
     }
 }
 
